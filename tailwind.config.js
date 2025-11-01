8b25fb26a91062c0713ddfbd5da8ddf79e2c690d
nitad54448 <ndragoe@gmail.com>
2025-11-01 18:11:19 +0100

    nov 1

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+content: [
+"./*.{html,js}" // Scans all .html and .js files in the root
+],
+theme: {
+extend: {},
+},
+pl
